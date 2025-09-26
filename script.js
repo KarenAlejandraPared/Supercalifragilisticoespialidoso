e955c91c3336f8ea4a6ebc56062c5c567445b760
KarenAlejandraPared <ykpared13@gmail.com>
2025-09-26 09:17:40 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+fetch('libros.json')
+  .then(response => response.json())
+  .then(libros => {
+    const secciones = {
+      "Derecho": document.getElementById("derecho"),
+      "Educación": document.getElementById("educacion"),
+      "Psicología": document.getElementById("psicologia"),
+      "Literatura Juvenil": document.getElementById("literatura"),
+      "Metodología": document.getElementById("metodologia")
+      "Filosofía": document.getElementById("metodologia")
+    };
+
+    libros.forEach(libro => {
+      const item = document.createElement("li");
+      item.innerHTML = `<strong>${libro.titulo}</strong> - ${libro.autor} (${libro.formato}) <a href="${libro.enlace}" target="_blank">📥 Descargar</a>`;
+      secciones[libro.categoria]?.querySelector("ul")?.appendChild(item);
+    });
+  });
